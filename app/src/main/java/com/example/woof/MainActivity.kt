cda41134d18d2d0fdf4eddb576dc569193260f8c
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-08-21 17:31:44 +0100

    Show or Hide the Hobby on Button Click

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -114,7 +115,6 @@ fun WoofApp() {
             items(dogs) {
                 DogItem(
                     dog = it,
-                    expanded = true,
                     modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
                 )
             }
@@ -150,9 +150,9 @@ fun DogHobby(
 @Composable
 fun DogItem(
     dog: Dog,
-    expanded: Boolean,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
     Card(modifier = modifier) {
         Row(
             modifier = Modifier
@@ -164,18 +164,20 @@ fun DogItem(
             Spacer(modifier = Modifier.weight(1f))
             DogItemButton(
                 expanded = expanded,
-                onClick = { /*TODO*/ }
+                onClick = { expanded = !expanded }
             )
         }
-        DogHobby(
-            dog.hobbies,
-            modifier = Modifier.padding(
-                start = dimensionResource(R.dimen.padding_medium),
-                top = dimensionResource(R.dimen.padding_small),
-                end = dimensionResource(R.dimen.padding_medium),
-                bottom = dimensionResource(R.dimen.padding_medium)
+        if (expanded) {
+            DogHobby(
+                dog.hobbies,
+                modifier = Modifier.padding(
+                    start = dimensionResource(R.dimen.padding_medium),
+                    top = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_medium),
+                    bottom = dimensionResource(R.dimen.padding_medium)
+                )
             )
-        )
+        }
     }
 }
 
@@ -190,7 +192,7 @@ private fun DogItemButton(
         modifier = modifier
     ) {
         Icon(
-            imageVector = Icons.Filled.ExpandMore,
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
             contentDescription = stringResource(R.string.expand_button_content_description),
             tint = MaterialTheme.colorScheme.secondary
         )
@@ -208,7 +210,6 @@ fun DogIcon(
     @DrawableRes dogIcon: Int,
     modifier: Modifier = Modifier
 ) {
-    var expanded by remember { mutableStateOf(false) }
     Image(
         modifier = modifier
             .size(dimensionResource(R.dimen.image_size))
